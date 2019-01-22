8e7bb8d084cf75e7bbe206d3455fee9f61446492
shumvlesu <37502809+shumvlesu@users.noreply.github.com>
2019-01-22 20:48:39 +0300

    Страница 130

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -1,6 +1,7 @@
 package com.hfad.mymessenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,9 @@ public class CreateMessageActivity extends Activity {
 
 
     public void onSendMessage(View view){
+
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
 
     }
 
