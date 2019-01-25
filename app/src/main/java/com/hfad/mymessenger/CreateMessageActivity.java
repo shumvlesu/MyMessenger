2aad3ba6427277f11bd274142bdfb88271e34511
shumvlesu <37502809+shumvlesu@users.noreply.github.com>
2019-01-25 22:23:23 +0300

    Страница 136

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -17,6 +18,9 @@ public class CreateMessageActivity extends Activity {
     public void onSendMessage(View view){
 
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        EditText messageView = findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
 
     }
--- app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/ReceiveMessageActivity.java
@@ -2,12 +2,20 @@ package com.hfad.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class ReceiveMessageActivity extends Activity {
+    public static final String EXTRA_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        String messageText = getIntent().getStringExtra(EXTRA_MESSAGE);
+        TextView  messageView = findViewById(R.id.message);
+        messageView.setText(messageText);
+
     }
 }
